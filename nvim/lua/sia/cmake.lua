dc44e8ecb7d9ddca094e0e03f0e1c599b9b17a9c
sia <slck.aydi@gmail.com>
2023-07-29 22:33:35 +0300

    Add Files

---
+++ nvim/lua/sia/cmake.lua
@@ -0,0 +1,15 @@
+local cmake_status_ok, cmake = pcall(require, "cmake")
+if not cmake_status_ok then
+    return
+end
+
+local path_status_ok, path = pcall(require, "plenary.path")
+if not path_status_ok then
+    return
+end
+
+cmake.setup({
+copy_compile_commands = false,
+build_dir = tostring(path:new('{cwd}', 'build')),
+configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
+})
